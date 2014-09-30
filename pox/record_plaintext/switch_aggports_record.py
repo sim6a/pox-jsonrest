3959e7da41c8f1c49a0d64b830d9edaad099ffa1
festradasolano <festradasolano@gmail.com>
2014-09-30 09:42:01 -0500

    Updated license details.

--- pox/record_plaintext/switch_aggports_record.py
+++ pox/record_plaintext/switch_aggports_record.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Felipe Estrada-Solano
+# Copyright 2014 Felipe Estrada-Solano <festradasolano at gmail>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
